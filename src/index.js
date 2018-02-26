6337c8c511a816a189c8b6e3cb72707974278ae8
zhekix <zhekix@gmail.com>
2018-02-27 00:50:36 +0300

    fourth task

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var math = require('mathjs');
+
+module.exports = function solveEquation(equation) {
+  // your implementation
+
+  var coef    = math.rationalize(equation,{},true).coefficients;
+  var a       = coef[2], b = coef[1], c = coef[0];
+  var d       = b*b - 4*a*c;
+  var solutions = [];
+  solutions.push( Math.round( ( -b + Math.sqrt(d) )/(2*a) ) );
+  solutions.push( Math.round( ( -b - Math.sqrt(d) )/(2*a) ) );
+  solutions.sort(compareNumeric);
+
+  return solutions;
+}
+
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
